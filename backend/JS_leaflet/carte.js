07c15b91ddae4f059ced7480338bbce83168acd6
Marie-T-code <thomas.marie8@gmail.com>
2025-07-16 14:37:40 +0200

    carte_base_ok

--- backend/JS_leaflet/carte.js
+++ backend/JS_leaflet/carte.js
@@ -1,15 +1,14 @@
 console.log('chemin ok carte.js loaded');
 
-var map = L.map('map').setView([46.98, 3.15], 15);
-const osm = L.titleLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var map = L.map('map').setView([46.99, 3.157], 15);
+const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     minZoom: 13, 
-    maxZoom: 16,
+    maxZoom: 18,
     attribution : '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-L.control.layers(baseMaps).addTo(map);
 
-fetch('fetch_evenements.php')
+fetch('/JS_leaflet/fetch_evenements.php')
 .then(response => {
     if(!response.ok){
         throw new Error(`erreur HTTP: ${response.status}`);
@@ -20,4 +19,48 @@ fetch('fetch_evenements.php')
     console.log('données reçues:', data);
     console.log('Type de data:', typeof data);
     console.log('data.features:', data.features);
+
+    if(!data.features || data.features.length === 0){
+        console.warn('aucune donnée trouvée'); 
+        alert('aucun evènement trouvé');
+        return;
+    }
+
+    const markers = L.markerClusterGroup();
+    
+    
+    const geojsonLayer = L.geoJSON(data, {
+        pointToLayer: function(feature, Latlng){
+            return L.marker(Latlng);
+        }, 
+
+        onEachFeature: function (feature, layer){
+            const nom = feature.properties.titre || 'sans nom'; 
+            const description = feature.properties.description || ''; 
+            const dates = feature.properties.dates || 'aucune date fournie'; 
+            const photo = feature.properties.image || 'inconnu'; 
+
+            let imgHTMLPhoto = ''; 
+            if (photo && photo !== 'inconnu') {
+                imgHTMLPhoto = `<img src="${photo}" alt="Image de ${nom}"
+                class="popup-img">`;
+            }
+
+            const popupContent = `
+                <div class="popup-content">
+                <h3>${nom}</h3>
+                <p>${description}</p>
+                <p>${dates}</p>
+                ${imgHTMLPhoto}
+                </div>
+            `;
+
+            layer.bindPopup(popupContent); 
+        }
+    });
+
+    markers.addLayer(geojsonLayer); 
+
+    map.addLayer(markers); 
 })
+.catch(error => console.error('Erreur de chargement GeoJson :', error)); 
--- backend/index.php
+++ backend/index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nevers Evènements</title>
+    <link rel="stylesheet" href="/CSS/carte.css">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster@1.5.3/dist/MarkerCluster.css" />
@@ -14,7 +15,7 @@
 <body>
 
 <div id="map">
-    
+
 </div>
 
 </body>
